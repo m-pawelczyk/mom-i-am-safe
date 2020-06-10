8e5a8b2639872a4331911005d90cebbbadb0ebe1
Michal Pawelczyk [t: @m_pawelczyk] <michal.pawelczyk@gmail.com>
2020-06-10 16:36:21 +0200

    inicial commit for mias-rest-gate project. closes #4
    update general draft. closes #4

---
+++ mias-rest-gate/src/main/java/pro/pawelczyk/miasrestgate/MiasRestGateApplication.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.miasrestgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiasRestGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiasRestGateApplication.class, args);
+    }
+
+}
---
+++ mias-rest-gate/src/test/java/pro/pawelczyk/miasrestgate/MiasRestGateApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.miasrestgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiasRestGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
