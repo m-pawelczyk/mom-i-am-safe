64d42ca5997215c928b67355007d47ca69f627c9
Michal Pawelczyk [t: @m_pawelczyk] <michal.pawelczyk@gmail.com>
2020-06-17 13:44:05 +0200

    create mias-core project. Closes #19

---
+++ mias-core/src/main/java/pro/pawelczyk/miascore/MiasCoreApplication.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.miascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiasCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiasCoreApplication.class, args);
+    }
+
+}
