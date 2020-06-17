8ac22f258c9e27b2e3fbca81590f1b41720dd016
Michal Pawelczyk [t: @m_pawelczyk] <michal.pawelczyk@gmail.com>
2020-06-17 14:25:50 +0200

    create mias-twitter-updater project. Closes #20

---
+++ mias-twitter-updater/src/main/java/pro/pawelczyk/miastwitterupdater/MiasTwitterUpdaterApplication.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.miastwitterupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiasTwitterUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiasTwitterUpdaterApplication.class, args);
+    }
+
+}
---
+++ mias-twitter-updater/src/test/java/pro/pawelczyk/miastwitterupdater/MiasTwitterUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.pawelczyk.miastwitterupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiasTwitterUpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
