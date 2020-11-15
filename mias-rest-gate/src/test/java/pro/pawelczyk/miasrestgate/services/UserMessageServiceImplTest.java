358e883a12b207053ecb7aac9154103fe673488a
Michal Pawelczyk <michal.pawelczyk@gmail.com>
2020-11-15 12:53:46 +0100

    Add integration test for RabbitMq with containers. Closes #48

--- mias-rest-gate/src/test/java/pro/pawelczyk/miasrestgate/services/UserMessageServiceImplTest.java
+++ mias-rest-gate/src/test/java/pro/pawelczyk/miasrestgate/services/UserMessageServiceImplTest.java
@@ -11,7 +11,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import pro.pawelczyk.miasrestgate.api.v1.model.AcceptedMessageDTO;
 import pro.pawelczyk.miasrestgate.api.v1.model.SMSMessageDTO;
 import pro.pawelczyk.miasrestgate.messages.UserMessageDTO;
+import pro.pawelczyk.miasrestgate.valueobjects.SenderType;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -43,6 +45,11 @@ class UserMessageServiceImplTest {
         // than
         assertEquals(SENDER_ID, acceptedMessageDTO.getSenderId());
         assertEquals(MESSAGE_TEXT, acceptedMessageDTO.getMessageText());
-        verify(rabbitTemplate, times(1)).convertAndSend(anyString(), Mockito.any(UserMessageDTO.class));
+        verify(rabbitTemplate, times(1)).convertAndSend(anyString(),
+                Mockito.eq(new UserMessageDTO(acceptedMessageDTO.getUuid(),
+                        acceptedMessageDTO.getTimestamp(),
+                        smsMessageDTO.getPhoneNumber(),
+                        SenderType.SMS,
+                        smsMessageDTO.getMessageText())));
     }
 }
---
+++ mias-rest-gate/src/test/java/pro/pawelczyk/miasrestgate/services/UserMessageServiceImplWithContainerTest.java
@@ -0,0 +1,94 @@
+package pro.pawelczyk.miasrestgate.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pro.pawelczyk.miasrestgate.api.v1.model.AcceptedMessageDTO;
+import pro.pawelczyk.miasrestgate.api.v1.model.SMSMessageDTO;
+import pro.pawelczyk.miasrestgate.config.RabbitConfig;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(classes = UserMessageServiceImplWithContainerTest.TestConfig.class)
+@ActiveProfiles("container")
+@Testcontainers
+public class UserMessageServiceImplWithContainerTest {
+
+    @Container
+    private static final RabbitMQContainer CONTAINER = new RabbitMQContainer()
+            .withReuse(true);
+
+    static {
+        CONTAINER.start();
+        System.setProperty("RABBIT_PORT", String.valueOf(CONTAINER.getFirstMappedPort()));
+    }
+
+    @Test
+    void shouldMessageContainPhoneNumberAndSimpleMessage(
+            @Autowired UserMessageService notifier,
+            @Autowired RabbitTemplate rabbitTemplate) {
+        // given
+        SMSMessageDTO smsMessageDTO = aSmsMessageDTO();
+        // when
+        AcceptedMessageDTO acceptedMessageDTO = notifier.createAndRedirectSMSMessage(smsMessageDTO);
+        // then
+        Message message = rabbitTemplate.receive(RabbitConfig.queueName, 100);
+        then(message).isNotNull();
+        System.out.println(messageBody(message));
+        then(messageBody(message)).contains(acceptedMessageDTO.getUuid())
+                .contains(smsMessageDTO.getPhoneNumber())
+                .contains(smsMessageDTO.getMessageText());
+    }
+
+    @Test
+    void shouldMessageContainPhoneNumberAndMessageWithGPSCoordEncoded(
+            @Autowired UserMessageService notifier,
+            @Autowired RabbitTemplate rabbitTemplate) {
+        // given
+        SMSMessageDTO smsMessageDTO = aSmsMessageDTOWithGPS();
+        // when
+        AcceptedMessageDTO acceptedMessageDTO = notifier.createAndRedirectSMSMessage(smsMessageDTO);
+        // then
+        Message message = rabbitTemplate.receive(RabbitConfig.queueName, 100);
+        then(message).isNotNull();
+        then(messageBody(message))
+                .contains(acceptedMessageDTO.getUuid())
+                .contains(smsMessageDTO.getPhoneNumber())
+                .contains(smsMessageDTO.getMessageText());
+    }
+
+    private SMSMessageDTO aSmsMessageDTO() {
+        return new SMSMessageDTO("500600700", "Mom I am safe!");
+    }
+
+    private SMSMessageDTO aSmsMessageDTOWithGPS() {
+        return new SMSMessageDTO("500600700", "Mom I am safe! Hello Mom! gps$-51.7293565,-72.510806,-10.7");
+    }
+
+    private String messageBody(Message message) {
+        return new String(message.getBody());
+    }
+
+    @Configuration
+    @Import(RabbitConfig.class)
+    @ImportAutoConfiguration(RabbitAutoConfiguration.class)
+    static class TestConfig {
+
+        @Bean
+        UserMessageService userMessageService (RabbitTemplate rabbitTemplate) {
+            return new UserMessageServiceImpl(rabbitTemplate);
+        }
+    }
+}
+
